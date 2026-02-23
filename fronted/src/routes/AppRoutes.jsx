8858d00bd64cf55399dd35eecc70b5226836af9d
Gauri Salunkhe <gaurisalunkhe.nanotechsoftwares@gmail.com>
2026-02-23 22:48:30 +0530

    routing for dashboards

--- fronted/src/routes/AppRoutes.jsx
+++ fronted/src/routes/AppRoutes.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../ pages / public/ Home";
+ import  Home from "../ pages / public/ Home";
 import Courses from ".. /pages/ public/ Courses";
 import CourseDetails from "../ pages/ public/ CourseDetails";
 import Login from "../ pages/ public/Login";
-import StudentDashboard from ".. / pages /student / Dashboard";
+ import StudentDashboard from ".. / pages /student / Dashboard";
 import InstructorDashboard from "../ pages/ instructor/ Dashboard";
-import AdminDashboard from "../ pages/ admin/ Dashboard";
+import  AdminDashboard from "../ pages/ admin/ Dashboard";
 
 function AppRoutes() {
   return (
     <Routes>
-      {/* Public  route */}
+      {/* Public  */}
       <Route path="/" element={<Home />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/courses/:id" element={< CourseDetails />} />
-      <Route path="/login" element={<Login />} />
+       <Route path="/courses" element={<Courses />} />
+      <Route path="/courses/:id" element={< CourseDetails />}  />
+    <Route path="/login" element={<Login />} />
 
-       {/* Student route */}
+                 {/* Student  */}
       <Route path="/student/dashboard" element={< StudentDashboard />} />
 
-      {/* Instructor route */}
-      <Route path="/instructor/dashboard" element={< InstructorDashboard />} />
+              {/* Instructor */}
+      <Route path="/instructor/dashboard" element={< InstructorDashboard  />} />
 
-      {/* Admin ka route */}
-      <Route path="/admin/dashboard" element={< AdminDashboard />} />
+             {/* Admin */}
+      <Route path="/admin/dashboard" element={<  AdminDashboard />} />
     </Routes>
   );
 }
