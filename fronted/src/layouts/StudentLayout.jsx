7f0d3d6e480a799f1f2a1f5d8b6ab28b1538c15f
Gauri Salunkhe <gaurisalunkhe.nanotechsoftwares@gmail.com>
2026-02-23 23:20:06 +0530

    Layouts

--- fronted/src/layouts/StudentLayout.jsx
+++ fronted/src/layouts/StudentLayout.jsx
@@ -3,12 +3,12 @@ import Sidebar from " ../components/layout/Sidebar";
 const StudentLayout = ( { children} )=> {
   return(
     <div>
-      <Navbar/>
+    <Navbar/>
       <div style= {{ display: "flex" }} >
-        <Sidebar />
-        <main style= {{ padding: "20px" }} >{children} </main>
+         <Sidebar />
+      <main style= {{ padding: "20px" }} >{children} </main>
       </div>
-    </div>
+   </div>
   );
 };
   
